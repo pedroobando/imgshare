5199e439a241e43d339d8a91c96bdf9da7b6237c
Pedro Obando <pedro.obando@gmail.com>
2018-12-22 17:04:04 -0400

    Delete image

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,12 +1,38 @@
+$('#post-comment').hide();
+$('#btn-toggle-comment').click(e => {
+  e.preventDefault();
+  $('#post-comment').slideToggle();
+});
+
 $('#btn-like').click(function(e) {
   e.preventDefault();
   let imgId = $(this).data('id');
   // console.log(imgId);
   $.post(`/images/${imgId}/like`)
     .done(data => {
-      console.log(data);
+      // console.log(data);
       $('.likes-count').text(data.likes);
     });
 });
 
 
+$('#btn-delete').click(function(e) {
+  e.preventDefault();
+  let $this = $(this);
+  const response = confirm('Are you sure you want to delete this image..?');
+  if (response) {
+    let imgId = $this.data('id');
+    // console.log(imgId);
+    $.ajax({
+      url: `/images/${imgId}`,
+      type: 'DELETE'
+    })
+    .done(function (dresult) {
+      console.log(dresult);
+      $this.removeClass('btn-danger').addClass('btn-success');
+      $this.find('i').removeClass('fa-times').addClass('fa-check');
+      $this.append('<span>Deleted!</span>');
+    });
+  }
+  // console.log(response);
+});
